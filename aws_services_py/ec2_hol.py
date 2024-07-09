e640abd01c691972f2d1b2cad283eacaf56e0318
Uddip Mitra <uddipmitra@gmail.com>
2024-07-09 00:35:38 -0400

    Create ec2_hol.py

---
+++ aws_services_py/ec2_hol.py
@@ -0,0 +1,26 @@
+'''
+This script manages Amazon EC2 instances using the Boto3 Python SDK
+'''
+# Import statements
+
+
+# Create ec2 resource and instance name
+
+
+# Store instance id
+
+
+# Check if instance that you are trying to create already exists
+# and only work with an instance that hasn't be terminated
+
+
+# Launch a new EC2 instance if it hasn't already been created
+
+
+# Stop an EC2 instance
+
+
+# Start an EC2 instance
+
+
+# Terminate an EC2 instance
