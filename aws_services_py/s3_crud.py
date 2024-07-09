a5d4242719ba1ecbd31aaf88b9ecd9bfad83311c
Uddip Mitra <uddipmitra@gmail.com>
2024-07-08 23:35:12 -0400

    Create s3_crud.py

---
+++ aws_services_py/s3_crud.py
@@ -0,0 +1,27 @@
+# Import the boto3 library
+import boto3
+
+# Instantiate a boto3 resource for S3 and name your bucket
+s3 = boto3.resource('s3')
+bucket_name = 'dct-crud_1'
+
+# Check if bucket exists
+# Create the bucket if it does NOT exist
+all_my_buckets = [bucket_name for bucket in s3.buckets.all()]
+if bucket_name not in all_my_buckets:
+    print(f'{bucket_name} bucket does not exist. Creating now...')
+    s3.create_bucket(Bucket=)
+# Create 'file_1' and 'file_2'
+# UPLOAD 'file_1' to the new bucket
+
+
+# READ and print the file from the bucket
+
+
+# UPDATE 'file_1' in the bucket with new content from 'file_2'
+
+
+# DELETE the file from the bucket
+
+
+# DELETE the bucket (the bucket should be empty.)
