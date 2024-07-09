546878fc4657aca4e86f69cc4eea19f6030f2996
Uddip Mitra <uddipmitra@gmail.com>
2024-07-09 00:06:56 -0400

    CRUD Operations on S3 using Boto3

--- aws_services_py/s3_crud.py
+++ aws_services_py/s3_crud.py
@@ -14,17 +14,29 @@ if bucket_name not in all_my_buckets:
     print(f'{bucket_name} bucket has been created.')
 else:
     print(f'{bucket_name} bucket already exists. No need to create new one.')
-# Create 'file_1' and 'file_2'
-# UPLOAD 'file_1' to the new bucket
 
+# Create 'file_1' and 'file_2'
+file_1 = 'file_1.txt'
+file_2 = 'file_2.txt'
+
+# UPLOAD 'file_1' to the new bucket
+s3.Bucket(bucket_name).upload_file(Filename=file_1, Key=file_1)
+print(f'{file_1} has been uploaded to {bucket_name} bucket.')
 
 # READ and print the file from the bucket
-
+obj = s3.Object(bucket_name, file_1)
+body = obj.get()['Body'].read().decode('utf-8')
+print(f'{file_1} content: {body}')
 
 # UPDATE 'file_1' in the bucket with new content from 'file_2'
-
+s3.Object(bucket_name, file_1).put(Body=open(file_2, 'rb'))
+obj = s3.Object(bucket_name, file_1)
+body = obj.get()['Body'].read().decode('utf-8')
+print(f'{file_1} content: {body}')
 
 # DELETE the file from the bucket
-
+s3.Object(bucket_name, file_1).delete()
 
 # DELETE the bucket (the bucket should be empty.)
+bucket = s3.Bucket(bucket_name)
+bucket.delete()
