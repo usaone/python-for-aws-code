8668617d168bd49a1ab9d65ba961cc06ee260e3d
Uddip Mitra <uddipmitra@gmail.com>
2024-07-09 01:43:08 -0400

    Create vpc_hol.py

---
+++ aws_services_py/vpc_hol.py
@@ -0,0 +1,21 @@
+'''
+Using Boto3 to create a VPC with a public subnet and a private subnet
+'''
+
+# Import statements
+
+# Create VPC
+
+
+# Create internet gateway
+
+
+
+# Create a route table and a public route
+
+
+
+# Create 3 subnets
+
+
+
