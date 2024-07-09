9453eebd7b3473152cb997fd67fe2d0e713f92bd
Uddip Mitra <uddipmitra@gmail.com>
2024-07-09 05:22:10 -0400

    Create rds_hol.py

---
+++ aws_services_py/rds_hol.py
@@ -0,0 +1,26 @@
+'''
+RDS Hands-On Lab Using Boto3
+'''
+import boto3
+import time
+
+# Instantiate a boto3 client for RDS
+
+
+# User defined variables
+# username
+# password
+# db_subnet_group
+# db_cluster_id
+
+
+# Create the DB Cluster
+
+
+# Wait for the DB cluster to become available
+
+
+# Modify the DB cluster. Update the scaling configuration for the cluster
+
+
+# Delete the DB cluster
