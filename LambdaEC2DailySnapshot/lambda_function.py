e63c1ba57e345846b1cbbb4bb729c8d2b76a19c8
Uddip Mitra <uddipmitra@gmail.com>
2024-07-11 03:58:56 -0400

    Working Local Execution of Lambda

--- LambdaEC2DailySnapshot/lambda_function.py
+++ LambdaEC2DailySnapshot/lambda_function.py
@@ -1,8 +1,33 @@
 import json
+import boto3
+import logging
+from datetime import datetime
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    ec2 = boto3.client('ec2')
+    current_date = datetime.now().strftime("%Y-%m-%d")
+
+    try:
+        response = ec2.create_snapshot(
+            VolumeId=event["volume_id"],
+            Description='My EC2 Snapshot',
+            TagSpecifications=[
+                {
+                    'ResourceType': 'snapshot',
+                    'Tags': [
+                        {
+                            'Key': 'Name',
+                            'Value': f'My EC2 Snapshot-{event["volume_id"]}-{current_date}'
+                        },
+                    ]
+                },
+            ],
+        )
+
+        logger.info(f'Successfully created snapshot: {json.dumps(response, default=str)}')
+
+    except Exception as e:
+        logger.error(f'Error creating snapshot: {str(e)}')
